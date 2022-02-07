42da36b12ba3f8e33b2e717af273b0acc54f3765
Victoria Hopso <victoria.hopso@gmail.com>
2022-02-07 16:47:31 +0100

    Start implementing textual ui

--- Main.c
+++ Main.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
    // printf() displays the string inside quotation
-   printf("Select one of the following operations:\n");
-   printf("s (schedule), l (list), c (cancel), x (exit)\n");
-   char chr;
-   printf("Enter a character: \n");
-   scanf("%c",&chr);   
-   printf("you typed: %c \n", chr);
-   //printf("%c", b);
+   while (1)
+   {
+      printf ("\n======================================================\n");
+      printf("Select one of the following operations:\n");
+      printf("s (schedule) | l (list) | c (cancel) | x (exit)\n");
+      printf ("======================================================\n");
+      char chr;
+      printf("Enter a character: ");
+      scanf("%c", &chr);
+      //printf("\nyou typed: %c ", chr);
+      //printf("%c", b);
+      
+      switch (chr){
+         case 's':
+            printf("You typed s\n");
+            break;
+
+         case 'l':
+            printf("You typed l\n");
+            break;
+
+         case 'c':
+            printf("You typed c\n");
+            break;
+
+         case 'x':
+            printf("good bye\n");
+            exit(0);
+
+         // operator doesn't match any option
+         default:
+            printf("Error! operator is not correct\n");
+            break;
+      }
+   }
 
    return 0;
 }
