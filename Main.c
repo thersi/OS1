c6056865f8b1a878ee402c6316d77fb1258889b4
Victoria Hopso <victoria.hopso@gmail.com>
2022-02-07 14:38:33 +0100

    Initial commit

--- Main.c
+++ Main.c
@@ -2,6 +2,13 @@
 int main()
 {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
+   printf("Select one of the following operations:\n");
+   printf("s (schedule), l (list), c (cancel), x (exit)\n");
+   char chr;
+   printf("Enter a character: \n");
+   scanf("%c",&chr);   
+   printf("you typed: %c \n", chr);
+   //printf("%c", b);
+
    return 0;
 }
