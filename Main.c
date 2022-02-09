557a92f8c3a08ad1372020dddef3b907d23172d7
Frida Fosli <frida.fosli@gmail.com>
2022-02-09 17:32:19 +0100

    Created time input to a tm struct

--- Main.c
+++ Main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void setAlarm(){ // could be boolean to return validation to main loop 
    int hrs, mins, seconds,i=0;
@@ -10,10 +11,44 @@ void setAlarm(){ // could be boolean to return validation to main loop
    printf("Enter Second: ");
    scanf("%d",&seconds);
 
-   //Validation of input here
-   //create time object/variable 
+   time_t now,file;
+   struct tm *date;
+
+   /* fill the tm structure with the current date/time */
+   time_t currenttime = time(NULL);
+   struct tm *tm_struct = localtime(&currenttime);
+   int yearnow = tm_struct->tm_year;
+   int monthnow = tm_struct->tm_mon;
+   int daynow = tm_struct->tm_mday;
+   int hournow = tm_struct->tm_hour;
+   int minnow = tm_struct->tm_min;
+   int secnow = tm_struct->tm_sec;
+
+   time(&now);
+   date = localtime(&now);
+   /* backfill some specific items in the date structure */
+   date->tm_sec = seconds;
+   date->tm_min = mins;
+   date->tm_hour = hrs;
+   date->tm_mday = daynow;
+   if (hrs < hournow) {
+      date->tm_mday = daynow + 1;
+   }
+   if (hrs == hournow && mins < minnow) {
+      date->tm_mday = daynow + 1;
+   }
+   if (hrs == hournow && mins == minnow && seconds < secnow) {
+      date->tm_mday = daynow + 1;
+   }
+   date->tm_mon = monthnow;
+   date->tm_year = yearnow;
+   /* convert the structure to a time_t value */
+   file = mktime(date);
+    
+   /* output results */
+   printf("Timer set at %s\n",ctime(&file));
+
    //add alarm to data structure of alarms
-   printf("TIMER SET AT: %d : %d : %d ",hrs,mins,--seconds);
 }
 
 void deleteAlarm(){ // could be boolean to return validation to main loop 
@@ -27,6 +62,9 @@ void deleteAlarm(){ // could be boolean to return validation to main loop
 }
 
 void listAlarms(){
+   time_t t = time(NULL);
+   printf("UTC:       %s", asctime(localtime(&t)));
+   
    //List all alarms in the data structure 
 }
 
